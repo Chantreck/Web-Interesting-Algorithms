e9905f779e83cd0402f5f75157404de5c1341fd6
Chantreck <vladnetaev@mail.ru>
2021-03-22 20:28:56 +0700

    design changes in field and range-input

--- task1/script.js
+++ task1/script.js
@@ -6,6 +6,7 @@ function markCell(event) {
 
 function generateField() {
     var tableSize = document.getElementById("chooseSize").value;
+    document.getElementById("fieldSize").innerText = tableSize;
     
     let field = new Array(tableSize);
     for (let i = 0; i < tableSize; i++){
@@ -15,16 +16,26 @@ function generateField() {
     let table = document.createElement("TABLE");
     let width = 0.4 * document.documentElement.clientWidth;
     table.width = width;
-    table.border = "1 solid black";
 
     for (let i = 0; i < tableSize; i++){
         row = table.insertRow(-1);
         for (let j = 0; j < tableSize; j++){
             let cell = row.insertCell(-1);
-            cell.id = "defaultCell";
+
+            if (i == 2 & j == 5) {
+                cell.id = "blockedCell";
+            }
+            else if (i == 4 & j == 1) {
+                cell.id = "startCell";
+            }
+            else if (i == 7 & j == 9) {
+                cell.id = "endCell";
+            }
+            else {
+                cell.id = "defaultCell";
+            }
             cell.setAttribute("row", i);
             cell.setAttribute("column", j);
-            //cell.addElementListener("click", markCell);
             cell.height = width / tableSize;
             //cell.innerHTML = `${i},${j}`;
         }
