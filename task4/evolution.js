eac11de4de90cc079da81fedc4fcf8031a3a4f9e
Chantreck <vladnetaev@mail.ru>
2021-03-28 23:10:33 +0700

    comments deleted

--- task4/evolution.js
+++ task4/evolution.js
@@ -6,7 +6,6 @@ var populationSize = 1000;
 var calculatedFitness = new Map();
 
 class Chromosome {
-    // constructor(parent1 = "Adam", parent2 = "Eve", genesArray) {
     constructor(genesArray) {
         if (genesArray) {
             this.genes = genesArray;
@@ -14,8 +13,6 @@ class Chromosome {
             this.genes = this.combinateGenes();
         }
         this.fitness = this.calculateFitness();
-        /* this.parent1 = parent1;
-        this.parent2 = parent2; */
     }
 
     combinateGenes() {
@@ -69,7 +66,6 @@ class Chromosome {
         let parent1Genes = parent1.genes;
         let parent2Genes = parent2.genes;
         let child = parent1Genes.slice();
-        //let swapFirstGene = 3;
         let swapFirstGene = getRandom(child.length - crossoverConstant + 1);
         let addedCities = [];
         let deletedCities = [];
@@ -86,17 +82,6 @@ class Chromosome {
         deletedCities = deletedCities.filter(item => {
             return addedCities.indexOf(item) < 0;
         });
-
-        /* let position = 0;
-        while (position < addedCities.length) {
-            let city = addedCities[position];
-            if (deletedCities.includes(city)) {
-                addedCities.splice(addedCities.indexOf(city), 1);
-                deletedCities.splice(deletedCities.indexOf(city), 1);
-                continue;
-            }
-            position++;
-        } */
     
         for (let i = 0; i < child.length; i++) {
             if (i >= swapFirstGene && i < swapFirstGene + crossoverConstant) continue;
@@ -107,12 +92,6 @@ class Chromosome {
             }
         }
         return new Chromosome(child);
-        /* try {
-            return new Chromosome(parent1, parent2, child);
-        }
-        catch {
-            console.log("error caught", parent1, parent2, child);
-        }    */
     }
 
     static selection(chromosome1, chromosome2) {
@@ -144,45 +123,6 @@ class Population extends Array {
             children.push(Chromosome.crossover(this[firstParent], this[secondParent]));
         }
 
-        /* if (time) {
-            let crossoverstart = Date.now();
-            for (let i = 0; i < populationSize; i++) {
-                let firstParent = getRandom(populationSize);
-                let secondParent;
-                do {
-                    secondParent = getRandom(populationSize);
-                } while (secondParent == firstParent);
-                children.push(Chromosome.crossover(this[firstParent], this[secondParent]));
-            }
-            console.log("> Crossover:", Date.now() - crossoverstart);
-        } else {
-            for (let i = 0; i < populationSize; i++) {
-                let firstParent = getRandom(populationSize);
-                let secondParent;
-                do {
-                    secondParent = getRandom(populationSize);
-                } while (secondParent == firstParent);
-                children.push(Chromosome.crossover(this[firstParent], this[secondParent]));
-            }
-        } */
-
-        /* if (time) {
-            let crossoverstart = Date.now();
-            for (let i = 0; i < populationSize - 1; i++) {
-                for (let j = i + 1; j < populationSize; j++) {
-                    children.push(Chromosome.crossover(this[i], this[j]));
-                }
-            }
-            console.log("> Crossover:", Date.now() - crossoverstart);
-        } else {
-            for (let i = 0; i < populationSize - 1; i++) {
-                for (let j = i + 1; j < populationSize; j++) {
-                    children.push(Chromosome.crossover(this[i], this[j]));
-                }
-            }
-        } */
-        
-        //let mutatedChildren = new Population();
         let childrenCount = children.length;
         let mutatedChildrenCount = Math.floor(populationSize * mutationRate);
 
@@ -190,21 +130,6 @@ class Population extends Array {
             let victim = getRandom(childrenCount);
             children[victim] = children[victim].mutation();
         }
-
-        /* if (time) {
-            let mutationstart = Date.now();
-            for (let i = 0; i < mutatedChildrenCount; i++) {
-                let victim = getRandom(childrenCount);
-                children[victim] = children[victim].mutation();
-            }
-            console.log("> Mutation:", Date.now() - mutationstart);
-        }
-        else {
-            for (let i = 0; i < mutatedChildrenCount; i++) {
-                let victim = getRandom(childrenCount);
-                children[victim] = children[victim].mutation();
-            }
-        } */
         
         return this.concat(children);
     }
@@ -260,78 +185,26 @@ function calculateDistances(cities) {
 export function evolution(cities, lines) {
     citiesCount = cities.length;
     crossoverConstant = getRandom(citiesCount / 2);
-    //console.log("CrossoverConstant", crossoverConstant);
     distances = calculateDistances(cities);
-
-    //console.log(distances);
 
     let generation = new Population();
     let newGeneration = new Population();
     newGeneration.makeGeneration(populationSize);
-
-    /* let newgenstart = Date.now();
-    newGeneration.makeGeneration(populationSize);
-    console.log("Making a generation:", Date.now() - newgenstart); */
-    //newGeneration.visualize(cities, lines);
-
-    //console.log("Before cycle", newGeneration);
-
     let generationNumber = 0;
 
     let evolutionStart = Date.now();
-
     while(!Population.isEqual(generation, newGeneration)) {
         generation = newGeneration.slice();
-        // console.log("start of cycle body", generation);
 
         newGeneration = generation.breeding();
-        /* if (generationNumber == 0) {
-            let breedingstart = Date.now();
-            newGeneration = generation.breeding(1);
-            console.log("Breeding a generation:", Date.now() - breedingstart);
-        } else {
-            newGeneration = generation.breeding(0);
-        } */
-        
-        //console.log("after breeding", newGeneration);   
 
         newGeneration = newGeneration.selection();
-        /* if (generationNumber == 0) {
-            let selectionstart = Date.now();
-            newGeneration = newGeneration.selection();
-            console.log("Selection a generation:", Date.now() - selectionstart);
-        } else {
-            newGeneration = newGeneration.selection();
-        } */
-        //console.log("after selection", newGeneration);
-        //newGeneration.visualize(cities, lines);
         generationNumber++;
     }
     
     console.log(`Evolution ended in ${Date.now() - evolutionStart} ms`);
     console.log(`Generations passed: ${generationNumber}`);
     console.log(`Solution:`, generation[0]);
-}
-
-export function test(cities, lines) {
-
-    let array = [5, 0, 6, 2, 3, 7, 8, 4, 1];
-    let concatslice = Date.now();
-    let result1 = deleteValue(array, 5);
-    console.log("Concat + slice", Date.now() - concatslice);
-
-    let splice = Date.now();
-    let result2 = array.splice(5, 1);
-    console.log("Splice", Date.now() - splice);
-
-    /* citiesCount = cities.length;
-    crossoverConstant = 3
-    distances = calculateDistances(cities);
-
-    let parent1 = new Chromosome("god", "god", [5, 0, 6, 2, 3, 7, 8, 4, 1]);
-    let parent2 = new Chromosome("god", "god", [5, 6, 4, 3, 7, 8, 1, 2, 0]);
-    debugger;
-    let child = Chromosome.crossover(parent1, parent2); */
 }
 
 import {Line} from '../dots.js';
