7284b378132551a0fad3b2ed40ac332706a83425
Chantreck <vladnetaev@mail.ru>
2021-03-28 23:04:05 +0700

    testButton added

--- task4/script.js
+++ task4/script.js
@@ -1,9 +1,11 @@
 window.addEventListener("load", () => {
     window.action = "default";
+    window.lineCollection = new DotCollection();
     window.dotsCollection = new DotCollection();
     window.backButton.onclick = () => location.href="../index.html";
     window.actionButtons.addEventListener("click", buttonClickHandler);
     window.startAlgorithmButton.addEventListener("click", startAlgorithm);
+    window.test.addEventListener("click", startTest);
 })
 
 function buttonClickHandler(event) {
@@ -24,14 +26,39 @@ function changeAction(actionName) {
 }
 
 function clearField() {
+    window.lineCollection = new DotCollection();
     window.dotsCollection = new DotCollection();
 }
 
 function startAlgorithm() {
-    /* var parent1 = "A B C D E F G H I J".split(' ');
-    var parent2 = "I J D A G H B F C E".split(' ');
-    crossover(parent1, parent2, 5); */
+    window.dotsCollection.add(145.20001220703125,237);
+    window.dotsCollection.add(282.20001220703125,85);
+    window.dotsCollection.add(386.20001220703125,301);
+    window.dotsCollection.add(393.20001220703125,137);
+    window.dotsCollection.add(127.20001220703125,120);
+    window.dotsCollection.add(246.20001220703125,307);
+    window.dotsCollection.add(277.20001220703125,191);
+    window.dotsCollection.add(397.20001220703125,219);
+    window.dotsCollection.add(338.20001220703125,98);
+    window.dotsCollection.add(198.20001220703125,95);
+    window.dotsCollection.add(323.20001220703125,307);
+    console.log(window.dotsCollection);
+    evolution(window.dotsCollection, window.lineCollection)
+}
+
+function startTest() {
+    window.dotsCollection.add(173.20001220703125, 229);
+    window.dotsCollection.add(270.20001220703125,101);
+    window.dotsCollection.add(448.20001220703125,170);
+    window.dotsCollection.add(346.20001220703125,283);
+    window.dotsCollection.add(235.20001220703125,295);
+    window.dotsCollection.add(143.20001220703125,79);
+    window.dotsCollection.add(475.20001220703125,380);
+    window.dotsCollection.add(298.20001220703125,179);
+    window.dotsCollection.add(395.20001220703125,75);
+    //window.dotsCollection.add();
+    test(window.dotsCollection, window.lineCollection)
 }
 
 import {DotCollection} from '../dots.js'
-// import {crossover} from './evolution.js'
+import {evolution, test} from './evolution.js'
