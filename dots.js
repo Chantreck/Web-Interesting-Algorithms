9da6482d25c0dfd10e08b12425ec04859464d4ba
Chantreck <vladnetaev@mail.ru>
2021-03-29 19:01:31 +0700

    garbage removed

--- dots.js
+++ dots.js
@@ -17,30 +17,6 @@ export class DotCollection extends Array {
     }
 }
 
-/* export class DotCollection extends Map {
-    search(x, y, radius) {
-        let dots = this.values();
-        for (let dot of dots) {
-            if ((dot.x - x)**2 + (dot.y - y)**2 < radius**2) {
-                return this.get(dot.x, dot.y);
-            }
-        }
-    }
-    get(x, y) {
-        return super.get(`${x},${y}`);
-    }
-    add(x, y) {
-        let coordId = `${x},${y}`;
-        if (this.search(x, y, 20) == undefined) {
-            this.set(coordId, new Dot(x, y));
-        }
-    }
-    remove(x, y) {
-        let target = this.search(x, y, 10);
-        if (target != undefined) this.delete(`${target.x},${target.y}`);
-    }
-} */
-
 class Dot {
     constructor(x, y) {
         this.x = x;
