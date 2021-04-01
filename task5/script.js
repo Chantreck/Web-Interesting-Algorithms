ea52700b308785849381a62f09d7eb2e869ed42a
Chantreck <vladnetaev@mail.ru>
2021-04-01 23:31:46 +0700

    добавлена обработка csv-текста и настройки

--- task5/script.js
+++ task5/script.js
@@ -1,1 +1,25 @@
-window.backButton.onclick = () => location.href="../index.html";
+var sample;
+
+window.addEventListener("load", () => {
+    window.enterSample.onclick = showSampleArea;
+    window.backButton.onclick = () => location.href="../index.html";
+})
+
+function showSampleArea() {
+    window.sampleBlock.style.display = "block";
+    window.saveSample.onclick = saveSample;
+}
+
+function saveSample() {
+    sample = normalizeData(window.sampleTextArea.value.split('\n').filter(str => str.length >= 1));
+    console.dir(sample);
+    window.sampleBlock.style.display = "none"; //перенести на нажатие "Сгенерировать дерево"
+}
+
+function normalizeData(strings) {
+    let result = [];
+    for (let string of strings) {
+        result.push(string.split(','));
+    }
+    return result;
+}
