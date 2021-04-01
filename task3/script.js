91dc1fedec2cd5c0475dcef429a17d9b7c28acf4
Chantreck <vladnetaev@mail.ru>
2021-04-02 00:48:01 +0700

    adding dots fix

--- task3/script.js
+++ task3/script.js
@@ -1,25 +1,31 @@
 window.addEventListener("load", () => {
     window.action = "default";
-    window.dotsCollection = new DotCollection();
+    window.lines = [];
+    window.dots = new DotCollection();
+
     window.backButton.onclick = () => location.href="../index.html";
-    window.actionButtons.addEventListener("click", buttonClickHandler);
+    window.actionButtons.addEventListener("click", (event) => changeAction(event.target.dataset.action));
     window.clearField.addEventListener("click", clearField);
-})
+});
 
 function buttonClickHandler(event) {
     changeAction(event.target.dataset.action);
 }
 
-let statusMap = new Map([['default', 'Не выбрано'], ['add', 'Добавление города'], ['remove', 'Удаление города']]);
-
 function changeAction(actionName) {
+    let statusMap = new Map([['default', 'Не выбрано'], ['add', 'Добавление городов'], ['remove', 'Удаление городов'], ['running', 'Эволюционирование']]);
     let label = document.getElementById("currentAction");
     window.action = actionName;
     label.innerText = statusMap.get(window.action);
 }
 
 function clearField() {
-    window.dotsCollection = new DotCollection();
+    window.dots = new DotCollection();
+    updateInfo();
+}
+
+function updateInfo() {
+    window.objectCount.innerText = window.dots.length;
 }
 
 import {DotCollection} from '../dots.js'
