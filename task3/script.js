4d99a52c04b8ca8e68238831bf0e9c5f13450ec8
Chantreck <vladnetaev@mail.ru>
2021-04-05 19:11:37 +0700

    visualization

--- task3/script.js
+++ task3/script.js
@@ -6,15 +6,31 @@ window.addEventListener("load", () => {
     window.backButton.onclick = () => location.href="../index.html";
     window.actionButtons.addEventListener("click", (event) => changeAction(event.target.dataset.action));
     window.clearField.addEventListener("click", clearField);
-    window.startAlgorithmButton.addEventListener("click", start);
+    window.startAlgorithmButton.addEventListener("click", startAlgorithm);
+    window.test.addEventListener("click", startTest);
+
+    window.field.addEventListener("click", () => {
+        if (window.action == "add" || window.action == "remove") {
+            clearSolution();
+        }
+    });
 });
 
-function buttonClickHandler(event) {
-    changeAction(event.target.dataset.action);
-}
+window.addEventListener("resize", () => {
+    if (window.action != "running") {
+        let size = window.field.clientWidth;
+        for (let dot of window.dots) {
+            if (dot.x > size - 10 || dot.y > size - 10) {
+                window.dots.remove(dot.x, dot.y);
+            }
+        }
+        window.lines = [];
+        updateInfo();
+    }
+});
 
 function changeAction(actionName) {
-    let statusMap = new Map([['default', 'Не выбрано'], ['add', 'Добавление городов'], ['remove', 'Удаление городов'], ['running', 'Эволюционирование']]);
+    let statusMap = new Map([['default', 'Не выбрано'], ['add', 'Добавление городов'], ['remove', 'Удаление городов'], ['running', 'Муравьи бегают']]);
     let label = document.getElementById("currentAction");
     window.action = actionName;
     label.innerText = statusMap.get(window.action);
@@ -22,7 +38,7 @@ function changeAction(actionName) {
 
 function clearSolution() {
     window.lines = [];
-    //showInfo("none");
+    showInfo("none");
 }
 
 function clearField() {
@@ -31,17 +47,24 @@ function clearField() {
     clearSolution();
 }
 
-
+function startCheck() {
+    if (window.dots.length <= 1) {
+        showError("block", "А в чем смысл?", "Должно быть не меньше двух городов");
+        return false;
+    }
+    showError("none");
+    return true;
+}
 
 function showInfo(state) {
     window.hiddenInfo.style.display = state;
 }
 
-function updateInfo( tourLength) {
+function updateInfo(iterationNumber, tourLength) {
+    window.objectCount.innerText = window.dots.length;
+    window.iterationNumber.innerText = iterationNumber;
     window.tourLength.innerText = tourLength;
 }
-import {DotCollection} from '../dots.js'
-import {Line} from '../dots.js';
 
 function visualize(cities, lines) {
     window.lines = [];
@@ -55,14 +78,24 @@ function visualize(cities, lines) {
     window.lines.push(new Line(cities[lastCity], cities[tour[0]]));
 }
 
+async function startAlgorithm() {
+    if (!startCheck()) return;
+    showInfo("block");
+    updateInfo();
+    buttonsActivity(true);
+    changeAction("running");
+
+    await start();
+    buttonsActivity(false);
+    changeAction("default");
+}
+
 var ants = [];
 var bestPathArr = [];
 var doots;
 var pheromone = [];
 
-
 var Ant = function () {
-
     this.x;
     this.y;
     this.currentPos;
@@ -71,122 +104,97 @@ var Ant = function () {
     this.path = [];
     this.dist = 0;
 
-
-    this.distance = function(x2,y2){
-        
-        return (Math.sqrt(Math.pow(this.x - x2,2)+Math.pow(this.y - y2,2)));
+    this.distance = function(x2, y2){
+        return (Math.sqrt(Math.pow(this.x - x2, 2) + Math.pow(this.y - y2, 2)));
     }
 }
-
-
-
-
-
 
 function calculate(a) {
+    var denominator = 0;
+    var numerator = 0;
 
-var denominator = 0;
-var numerator = 0;
+    for(var i = 0; i < doots.length; i++){
+        if(a.visited[i] == 0){
+            var r = a.distance(doots[i].x,doots[i].y );
+            doots[i].tendency = 1 / r;
+            denominator += (Math.pow(pheromone[a.currentPos][i],1.5) * Math.pow(doots[i].tendency,2));
 
-for(var i = 0; i < doots.length; i++){
-
-    if(a.visited[i] == 0){
-
-        var r = a.distance(doots[i].x,doots[i].y );
-        doots[i].tendency = 1 / r;
-        denominator += (Math.pow(pheromone[a.currentPos][i],1.5) * Math.pow(doots[i].tendency,2));
-
+        }
+        else {
+            doots[i].tendency = 0;
+        }
     }
-    else{
-        doots[i].tendency = 0;
-    }
-}
 
-for(var i = 0; i < doots.length; i++){
-    
-    if(a.visited[i] == 0){
-        numerator = Math.pow(pheromone[a.currentPos][i],1.5) * Math.pow(doots[i].tendency,2);
-
-        doots[i].chance = (numerator / denominator);
+    for (var i = 0; i < doots.length; i++) {
+        if (a.visited[i] == 0){
+            numerator = Math.pow(pheromone[a.currentPos][i], 1.5) * Math.pow(doots[i].tendency, 2);
+            doots[i].chance = (numerator / denominator);
+        }
+        else {
+            doots[i].chance = 0;
+        }
     }
-    else{
-        doots[i].chance = 0;
-    }
-}
 }
 
 function findNextNode(a) {
+    var maxX;
+    var maxY;
+    var indf = -1;
+    var nextRand = Math.random();
 
-var maxX;
-var maxY;
-var indf = -1;
-
-var nextRand = Math.random();
-
-for(var i = 0; i < doots.length; i++){
-
-    if(a.visited[i] == 0 && nextRand < doots[i].chance){
-
-        maxX = doots[i].x;
-        maxY = doots[i].y;
-        indf = i;
-        break;
+    for (var i = 0; i < doots.length; i++){
+        if (a.visited[i] == 0 && nextRand < doots[i].chance){
+            maxX = doots[i].x;
+            maxY = doots[i].y;
+            indf = i;
+            break;
+        }
+        else if (nextRand >= doots[i].chance){
+            nextRand -= doots[i].chance;
+        }
     }
-    else if (nextRand >= doots[i].chance){
-        nextRand -= doots[i].chance;
+
+    for (var i = 0; i < doots.length; i++){
+        doots[i].pheromone *= 0.7;
+    }
+
+    if (indf != -1){
+        a.dist += a.distance(doots[indf].x,doots[indf].y);
+        a.x = maxX;
+        a.y = maxY;
+        a.currentPos = indf;
+        a.path.push(indf);
+        a.visited[indf] = 1;
     }
 }
 
-for(var i = 0; i < doots.length; i++){
-    doots[i].pheromone *= 0.7;
-}
+async function start() {
+    doots = window.dots.slice();
+    ants = [];
+    bestPathArr = [];
+    doots;
+    pheromone = [];
 
-if(indf != -1){
-    a.dist += a.distance(doots[indf].x,doots[indf].y);
-    a.x = maxX;
-    a.y = maxY;
-    a.currentPos = indf;
-    a.path.push(indf);
-    a.visited[indf] = 1;
-    
-}
-
-}
-
-
-function start() {
-doots = window.dots.slice();
-ants = [];
-bestPathArr = [];
-doots;
-pheromone = [];
-
-  var isStarted = true;
-  var bestPath = 999999999;
+    var isStarted = true;
+    var bestPath = Infinity;
     var ind = -1;
 
-    for(let i = 0; i < doots.length; i++){
+    for (let i = 0; i < doots.length; i++){
         var a = new Ant();
-
         ants.push(a);
     }
 
-   
-
-
     pheromone.length = doots.length;
 
-    for(var i = 0; i < doots.length; i++){
+    for (var i = 0; i < doots.length; i++) {
         pheromone[i] = [];
-        for(var j = 0; j < doots.length; j++){
+        for (var j = 0; j < doots.length; j++) {
             pheromone[i][j] = 0.2;
         }
     }
-    
 
-    for(var k = 0; k < 1000; k++){
-        for(var j = 0; j < ants.length; j++){
-
+    for (var k = 0; k < 100; k++) {
+        for (var j = 0; j < ants.length; j++) {
             ants[j].visited.length =  doots.length;
             ants[j].visited.fill(0);
             ants[j].visited[j] = 1;
@@ -198,13 +206,10 @@ pheromone = [];
             ants[j].currentPos= j;
             ants[j].path.push(j);
             
-
-            for(var i = 0; i < doots.length; i++){
+            for (var i = 0; i < doots.length; i++){
                 calculate(ants[j]);
                 findNextNode(ants[j]);
             }
-            
-            
 
             ants[j].path.push(j);
             ants[j].dist +=  ants[j].distance(doots[j].x,doots[j].y);
@@ -212,28 +217,36 @@ pheromone = [];
             if (bestPath >= ants[j].dist){
                 bestPath = ants[j].dist;
                 ind = j;
-                
                 bestPathArr = ants[j].path;
             }
         }
 
-        for(var i = 0; i < doots.length; i++){
-            for(var j = 0; j < doots.length; j++){
+        for (var i = 0; i < doots.length; i++){
+            for (var j = 0; j < doots.length; j++){
                 pheromone[i][j] *= 0.8;
             }
         }
     
-        for(var j = 0; j < ants.length; j++){
-            for(var i = 0; i < ants[j].path.length - 1; i++){
+        for (var j = 0; j < ants.length; j++){
+            for (var i = 0; i < ants[j].path.length - 1; i++){
                 pheromone[ants[j].path[i]][ants[j].path[i + 1]] += 4/ants[j].dist;
                 pheromone[ants[j].path[i + 1]][ants[j].path[i]] += 4/ants[j].dist;
             }
-    
             ants[j].dist = 0;
         }
+
+        visualize(doots, lines);
+        updateInfo(k, bestPath);
+        await sleep(100);
     }
-    visualize(doots, lines);
     isStarted = false;
-    console.log(bestPath);
-    console.log(bestPathArr);
 }
+
+function startTest() {
+    window.dots = test2;
+    startAlgorithm();
+}
+
+import {DotCollection, Line} from '../dots.js'
+import {showError, buttonsActivity, sleep} from '../general.js'
+import {test, test2} from '../task4/benchmark.js'
