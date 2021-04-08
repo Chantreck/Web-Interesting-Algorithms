3885ee4f42d5ae8a7bae562aa237508bec8e9b5a
Chantreck <vladnetaev@mail.ru>
2021-04-08 20:19:09 +0700

    default mode field implementation

--- task6/script.js
+++ task6/script.js
@@ -1,1 +1,100 @@
-window.backButton.onclick = () => location.href="../index.html";
+var cells;
+var pixels;
+var mode;
+
+window.addEventListener("load", () => {
+    cells = [];
+    for (let i = 0; i < 5; i++) {
+        cells[i] = [];
+        for (let j = 0; j < 5; j++) {
+            cells[i][j] = 0;
+        }
+    }
+
+    selectDefaultMode();
+    /* window.defaultMode.onclick = selectDefaultMode;
+    window.bonusMode.onclick = selectBonusMode; */
+    
+    window.clearField.onclick = clearField;
+
+    window.backButton.onclick = () => location.href="../index.html";
+})
+
+function selectDefaultMode() {
+    mode = "default";
+    window.field.style.display = "none";
+    window.table.style.display = "block";
+    generateTable(5, window.table);
+}
+
+function selectBonusMode() {
+    mode = "bonus";
+    window.table.style.display = "none";
+    window.field.style.display = "block";
+
+    generateField(50, window.field);
+}
+
+function generateTable(tableSize, tablePlaceHolder) {
+    let table = document.createElement("TABLE");
+    let width = tablePlaceHolder.clientWidth;
+    table.width = width;
+
+    for (let i = 0; i < tableSize; i++){
+        let row = table.insertRow(-1);
+        for (let j = 0; j < tableSize; j++){
+            let cell = row.insertCell(-1);
+            cell.dataset.state = cells[i][j];
+            cell.dataset.row = i;
+            cell.dataset.column = j;
+            cell.height = width / tableSize;
+        }
+    }
+
+    table.addEventListener("click", markCell);
+    tablePlaceHolder.innerHTML = "";
+    tablePlaceHolder.appendChild(table);
+}
+
+function generateField(tableSize, tablePlaceHolder) {
+    let table = document.createElement("TABLE");
+    let width = tablePlaceHolder.clientWidth;
+    table.width = width;
+
+    for (let i = 0; i < tableSize; i++){
+        let row = table.insertRow(-1);
+        for (let j = 0; j < tableSize; j++){
+            let cell = row.insertCell(-1);
+            cell.dataset.state = pixels[i][j];
+            cell.dataset.row = i;
+            cell.dataset.column = j;
+            cell.height = width / tableSize;
+        }
+    }
+
+    table.addEventListener("click", markCell);
+    tablePlaceHolder.innerHTML = "";
+    tablePlaceHolder.appendChild(table);
+}
+
+function markCell(event) {
+    let cell = event.target;
+    cell.dataset.state = (+cell.dataset.state) ? 0 : 1;
+    cells[cell.dataset.row][cell.dataset.column] = cell.dataset.state;
+}
+
+function clearField() {
+    switch (mode) {
+        case 'default':
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    cells[i][j] = 0;
+                }
+            }
+            generateTable(5, window.table);
+            break;
+
+        case 'bonus':
+
+    }
+}
